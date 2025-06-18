8174ed8f292cef22e69e66be0073400087ef87d5
Zacharia Ndinguri <zacthuku@gmail.com>
2025-06-18 20:39:50 +0300

    frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useState } from 'react'
+import './App.css'
+import { BrowserRouter } from 'react-router-dom'
+
+function App() {
+  return (
+    <BrowserRouter>
+    </BrowserRouter>
+  );
+  
+}
+
+export default App
